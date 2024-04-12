babf0e039d2a599f98e095be0011ba4237655e9a
Inhwa Kim <radiantbeing99@gmail.com>
2024-04-13 01:53:18 +0900

    refactor: 양식 초기화 함수 분리

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -24,7 +24,7 @@ const render = () => {
           <label>
             Font Family
             <select name="font-family">
-              <option value="">기본값</option>
+              <option selected value="">기본값</option>
               <optgroup label="Sans Serif">
                 ${fontFamilies.sansSerif.map(
                   (fontFamily) => /* HTML */ `<option value="${fontFamily}">
@@ -48,18 +48,28 @@ const render = () => {
   `;
 };
 
-const initializeForm = async () => {
+const addFormSubmitHandler = async () => {
   const _form = document.querySelector("form");
 
   _form.addEventListener("submit", async (e) => {
     e.preventDefault();
+
     const formData = new FormData(e.target);
     const config = Object.fromEntries(formData);
     await chrome.storage.sync.set(config);
     console.log("[MKRF] 설정이 저장되었습니다.", config);
   });
+};
 
-  const config = await chrome.storage.sync.get(["font-family"]);
+const restoreFormValue = async () => {
+  const _form = document.querySelector("form");
+  const config = await chrome.storage.sync.get();
+  const isEmpty = Object.keys(config).length === 0;
+
+  if (isEmpty) {
+    console.log("[MKRF] 저장된 설정이 없습니다.", config);
+    return;
+  }
   for (const key in config) {
     _form[key].value = config[key];
   }
@@ -67,4 +77,5 @@ const initializeForm = async () => {
 };
 
 render();
-await initializeForm();
+await addFormSubmitHandler();
+await restoreFormValue();
