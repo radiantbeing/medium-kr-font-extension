cdc69b15eda4fbf9f3026ec5fddeef8bd0c4f261
Inhwa Kim <radiantbeing99@gmail.com>
2024-05-18 12:09:30 +0900

    refactor: 주석 추가, 매개변수 및 프로퍼티 순서 변경

--- src/libs/fonts.ts
+++ src/libs/fonts.ts
@@ -1,41 +1,47 @@
+/**
+ * Medium KR Font 확장 프로그램에서 사용하는 글꼴의 타입입니다.
+ */
 export type Font = {
-  name: string;
   value: string;
+  name: string;
   style: "sans-serif" | "serif";
 };
 
+/**
+ * Medium KR Font 확장 프로그램에서 사용하는 글꼴의 배열입니다.
+ */
 export const fonts: Font[] = [
   {
-    name: "IBM Plex Sans KR",
     value: "IBM Plex Sans KR",
+    name: "IBM Plex Sans KR",
     style: "sans-serif",
   },
   {
-    name: "나눔바른고딕",
     value: "Nanum Barun Gothic",
+    name: "나눔바른고딕",
     style: "sans-serif",
   },
-  { name: "나눔고딕", value: "Nanum Gothic", style: "sans-serif" },
+  { value: "Nanum Gothic", name: "나눔고딕", style: "sans-serif" },
   {
-    name: "나눔스퀘어 네오",
     value: "NanumSquare Neo variable",
+    name: "나눔스퀘어 네오",
     style: "sans-serif",
   },
   {
-    name: "Noto Sans Korean",
     value: "Noto Sans KR",
+    name: "Noto Sans Korean",
     style: "sans-serif",
   },
   {
-    name: "Pretendard",
     value: "Pretendard Variable",
+    name: "Pretendard",
     style: "sans-serif",
   },
   {
-    name: "스포카 한 산스 네오",
     value: "Spoqa Han Sans Neo",
+    name: "스포카 한 산스 네오",
     style: "sans-serif",
   },
-  { name: "Noto Serif Korean", value: "Noto Serif KR", style: "serif" },
-  { name: "리디바탕", value: "RIDIBatang", style: "serif" },
+  { value: "Noto Serif KR", name: "Noto Serif Korean", style: "serif" },
+  { value: "RIDIBatang", name: "리디바탕", style: "serif" },
 ];
