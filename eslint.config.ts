eccc16209f84ade3d71ccdb421e71d185f99e7dc
김인화 <radiantbeing99@gmail.com>
2025-06-22 22:03:56 +0900

    `eslint-plugin-react` 패키지 제거

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,6 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default tseslint.config(
@@ -10,7 +9,6 @@ export default tseslint.config(
     },
     js.configs.recommended,
     tseslint.configs.recommended,
-    pluginReact.configs.flat.recommended,
     {
         files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
         languageOptions: {
