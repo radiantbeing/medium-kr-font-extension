78257825fc58219adece79373d2d559fd0b51a7d
김인화 <radiantbeing99@gmail.com>
2025-06-22 23:47:42 +0900

    타입 안전한 확장 프로그램 환경 설정 get/set 함수 모듈 추가

--- src/lib/settings.ts
+++ src/lib/settings.ts
@@ -1,24 +1,41 @@
 /**
- * 스토리지에 저장되는 설정 객체의 타입입니다.
+ * 확장 프로그램의 환경 설정 객체 타입입니다.
  */
-export type Settings = {
+
+type Settings = {
     "font-family"?: string;
 };
 
 /**
- * 스토리지에서 설정 객체를 가져옵니다.
- * @returns {Promise<Settings>} 설정 객체
+ * @deprecated 더 나은 타입 추론을 제공하는 `setSettingsV2` 함수 사용을 권장합니다.
  */
-export const getSettings = (): Promise<Settings> => {
-    return chrome.storage.sync.get() as Promise<Settings>;
-};
 
-/**
- * 스토리지에 새로운 설정 객체를 저장합니다.
- * @param settings 새로운 설정 객체
- * @returns {Promise<Settings>} 갱신된 설정 객체
- */
-export const setSettings = async (settings: Settings): Promise<Settings> => {
+async function setSettings(settings: Settings): Promise<Settings> {
     await chrome.storage.sync.set(settings);
     return await getSettings();
-};
+}
+
+async function getSettings(): Promise<Settings> {
+    return await chrome.storage.sync.get();
+}
+
+async function getSetting<K extends keyof Settings>(
+    name: K
+): Promise<Settings[K]> {
+    return (await chrome.storage.sync.get(name))[name];
+}
+
+async function setSettingsV2<K extends keyof Settings>(
+    name: K,
+    value: Settings[K]
+) {
+    await chrome.storage.sync.set({[name]: value});
+    return getSetting(name);
+}
+
+export {type Settings, getSettings, setSettings};
+export default Object.freeze({
+    get: getSetting,
+    getAll: getSettings,
+    set: setSettingsV2
+});
