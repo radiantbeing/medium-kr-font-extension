ef1fdb1913a6277ea18b33b6ad008939dece2aed
김인화 <radiantbeing99@gmail.com>
2025-06-23 17:19:46 +0900

    `onInstalled` listener의 화살표 함수 제거

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -3,7 +3,8 @@ import extensionSettings from "./lib/settings";
 /**
  * 확장 프로그램 설치 시 설정 객체를 초기화합니다.
  */
-chrome.runtime.onInstalled.addListener(async () => {
+
+chrome.runtime.onInstalled.addListener(async function () {
     const settings = await extensionSettings.getAll();
     if (!("font-family" in settings)) {
         await extensionSettings.set("font-family", "");
