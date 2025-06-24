e77cc9144ff57e6a5c26d4b6ac663312a4be130d
김인화 <radiantbeing99@gmail.com>
2025-06-24 21:28:29 +0900

    확장 프로그램 활성화 정보 문구 및 스타일 변경

--- src/popup/components/Indicator.tsx
+++ src/popup/components/Indicator.tsx
@@ -3,5 +3,9 @@ import useMediumTabActive from "../hooks/use-medium-tab-active";
 
 export default function Indicator(): JSX.Element {
     const isMediumTab = useMediumTabActive();
-    return <p>{isMediumTab ? "" : "접속 중인 탭은 Medium이 아닙니다."}</p>;
+    return isMediumTab ? (
+        <p className="indicator-active">확장 프로그램이 작동 중입니다.</p>
+    ) : (
+        <p className="indicator-inactive">현재 탭은 Medium이 아닙니다.</p>
+    );
 }
